4ce89066c44f1e5ff1461896933b8c1724122fca
Kevin Jiang <kev2018@mit.edu>
2020-10-08 17:16:31 -0400

    Add About Site Info

--- app/views/Index.js
+++ app/views/Index.js
@@ -9,7 +9,7 @@ const Index = () => (
       <header>
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
-          <p>A beautiful, responsive, react app written with modern Javascript.</p>
+          <p>Built during my virtual Zoom semester due to COVID-19. Courtesy of Michael D'Angelo's amazing <a href="https://github.com/mldangelo/personal-site">template</a>.</p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
@@ -19,7 +19,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/kev2010/personal-site">here</a>.</p>
     </article>
   </Main>
 );
