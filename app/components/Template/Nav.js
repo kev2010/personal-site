5f14f30a0e0e015a065b4a00c4471450517a4a35
Kevin Jiang <kev2018@mit.edu>
2020-10-08 17:16:12 -0400

    Change Sidebar Information

--- app/components/Template/Nav.js
+++ app/components/Template/Nav.js
@@ -12,17 +12,16 @@ const Nav = () => (
         <img src={`${BASE_PATH}/images/me_icon.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Kevin Jiang</h2>
+        <p><a href="mailto:cloudwiz24@gmail.com">cloudwiz24@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>, <a href="https://planet.com">Planet</a>, <a href="https://planetaryresources.com">Planetary Resources</a>, <a href="https://facebook.com">Facebook</a>, and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I'm Kevin! I'm an <a href="https://web.mit.edu/">MIT</a> undergrad interested in entrepreneurship, ML/AI, and SWE :). 
+        I'm currently working at <a href="https://www.odaportal.com/">Oda</a> (YC W20).
+        Before Oda, I was at <a href="https://instrumental.com/">Instrumental</a>, <a href="https://mitsloan.mit.edu/">MIT Sloan School of Management</a>, <a href="https://quest.mit.edu/">MIT Quest for Intelligence</a>, <a href="http://www.rle.mit.edu/">MIT RLE</a>, and <a href="https://www.ford.com/">Ford</a>.
       </p>
       <ul className="actions">
         <li>
@@ -41,7 +40,7 @@ const Nav = () => (
           </li>
         ))}
       </ul>
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Kevin Jiang <Link to="/">https://github.com/kev2010/personal-site</Link>.</p>
     </section>
   </section>
 );
