293edb3921c240b0a6cd4b690a87042775275f1c
Kevin Jiang <kev2018@mit.edu>
2020-10-13 19:44:03 -0400

    Update degrees.js

--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,14 +1,14 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Massachusetts Institute of Technology',
+    degree: 'Pursuing B.S. 6-3 Computer Science and Engineering',
+    link: 'https://mit.edu/',
+    year: 2022,
   }, {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Detroit Country Day School',
+    degree: 'High School Diploma',
+    link: 'https://www.dcds.edu/',
+    year: 2018,
   },
 ];
 
