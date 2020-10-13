54843c19870d65a54a6a6a46805d698f6ffd5f62
Kevin Jiang <kev2018@mit.edu>
2020-10-13 19:44:04 -0400

    Update positions.js

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -1,101 +1,57 @@
 const positions = [{
-  company: 'Arthena',
-  position: 'Co-founder, C.T.O.',
-  link: 'https://arthena.com',
-  daterange: 'January 2014 - Present',
+  company: 'Oda (YC W20)',
+  position: 'Machine Learning Engineer Intern',
+  link: 'https://www.odaportal.com/',
+  daterange: 'September 2020 - Present',
   points: [
-    'Deployed quantitative strategies to predict the value of fine art in various pricing contexts.',
-    'Built production, on-line, end-to-end optimized machine learning pipelines with Pandas, Numpy, Scikit, Tensorflow, Dagster, Postgres, etc. on GCP with Kubernetes.',
-    'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-    'YC Alumn. Worked on everything. Built a 16-person team. Operated company for 6+ years.',
+    'Developing ML/AI and NLP algorithms to help people find the best rentals'
   ],
 }, {
-  company: 'Skeptical Investments',
-  position: 'Co-founder',
-  link: 'http://skepticalinvestments.biz',
-  daterange: 'April 2017 - Present',
+  company: 'Instrumental',
+  position: 'Machine Learning Engineer Intern',
+  link: 'https://instrumental.com/',
+  daterange: 'May 2020 - August 2020',
   points: [
-    'Founded a micro-vc fund to throw token amounts of money at problems I find interesting.',
-    'Created the InstaSafe. Fast, automated investment docs for YC companies.',
+    'Built ML/CV visualization algorithms for explanation using Python, PyTorch, sklearn, and Jupyter',
+    'Developed features used by the ML and Product team and being considered for production to users'
   ],
 }, {
-  company: 'Enveritas',
-  position: 'Consultant',
-  link: 'http://enveritas.org',
-  daterange: 'September 2016 - 2018',
+  company: 'MIT Sloan School of Management',
+  position: '15.S50 Poker Theory Instructor',
+  link: 'https://mitsloan.mit.edu/',
+  daterange: 'January 2020 - February 2020',
   points: [
-    'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
-    'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
-    'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM’s, and other techniques.',
+    'Taught an MIT graduate seminar on poker fundamentals and topics including Nash equilibrium, game theory optimal, and exploitative play',
+    'Received from students a teacher evaluation rating of 6.9/7.0'
   ],
 }, {
-  company: 'Zenysis',
-  position: 'Consultant',
-  link: 'https://zenysis.com',
-  daterange: 'February 2016 - March 2016',
+  company: 'MIT Brain and Cognitive Sciences',
+  position: 'Researcher for MIT Quest for Intelligence',
+  link: 'https://bcs.mit.edu/',
+  daterange: 'July 2019 - September 2019',
   points: [
-    'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
-    'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
+    'Analyzed functional segregation of face and object processing in dual-task DCNNs',
+    'Processed training and test image data to run on various AlexNet and VGG16 models implemented in PyTorch'
   ],
 }, {
-  company: 'Matroid',
-  position: 'Co-founder',
-  link: 'https://matroid.com',
-  daterange: 'July 2015 - January 2016',
+  company: 'MIT Research Laboratory of Electronics',
+  position: 'Researcher',
+  link: 'http://www.rle.mit.edu/',
+  daterange: 'January 2019 - September 2019',
   points: [
-    'Developed end to end machine learning pipeline to train visual classifiers from keywords using Node.JS, Express, Keystone, MongoDB, AWS, S3, Caffe, and other technologies.',
-    'Received Series A term sheets for 20M+ valuations.',
+    'Analyzed speech samples using speech signal processing, linguistics, and human speech recognition',
+    'Developed a database for evaluating a prototype automatic speech analysis system using Praat'
   ],
 }, {
-  company: 'Planet',
-  position: 'Missions Intern',
-  link: 'https://planet.com',
-  daterange: 'June 2014 - January 2015',
+  company: 'Ford Motor Company',
+  position: 'Software Engineering Intern',
+  link: 'https://www.ford.com/',
+  daterange: 'May 2018 - July 2018',
   points: [
-    'Built models to improve image quality, signal to noise ratio, and dynamic range.',
-    'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
-    'Organized first hackathon, prototyped hardware, and designed and built photography equipment for rocket launches.',
+    'Created an algorithm in MATLAB to detect Global Navigation Satellite System inaccuracy in vehicle-to-vehicle communication for autonomous and self-driving cars',
+    'Patent pending for novel idea’s advantages over traditional methods'
   ],
-}, {
-  company: 'Planetary Resources',
-  position: 'Avionics Intern',
-  link: 'http://planetaryresources.com',
-  daterange: 'January 2014 - May 2014',
-  points: [
-    'Developed simulations in Matlab for Attitude Determination and Control Subsystem.',
-    'Developed processes for in lab testing and characterization of various subsystems.',
-    'Assembled flight hardware in cleanroom.',
-  ],
-}, {
-  company: 'Facebook',
-  position: 'Intern',
-  link: 'https://facebook.com',
-  daterange: 'June 2013 - September 2013',
-  points: [
-    'Developed software in python for automated testing of servers.',
-    'Performed statistical analysis with R, HIVE to assist in triage of malfunctioning servers.',
-    'Worked with vendors and ODM’s during triage to assist in risk mitigation.',
-  ],
-}, {
-  company: 'SEDS-USA',
-  position: 'At Large Board Member',
-  link: 'http://seds.org',
-  daterange: 'October 2013 - October 2014',
-  points: [
-    'Elected to Board of Directors of the USA’s largest student space advocacy group based on 5+ years of work with SEDS.',
-    'Responsibilities included: organizational strategy, conference presentations, fundraising, special projects, promoting SEDS nationally and internationally, and photographing conferences.',
-  ],
-},
-{
-  company: 'UB Nanosatellite Program',
-  position: 'Co-founder, Program Manager',
-  link: 'https://ubnl.space/',
-  daterange: 'October 2010 – June 2012',
-  points: [
-    'Coauthored grant to build a multi-spectral imaging satellite as part of the AFRL University Nanosatellite Program.',
-    'Lead a team of 60 students through satellite development life cycle. Served as a technical expert, acquired intimate working knowledge of satellite subsystems.  Solicited funding through NASA, AFOSR, and several corporate sponsors.',
-  ],
-},
+}
 ];
 
 export default positions;
